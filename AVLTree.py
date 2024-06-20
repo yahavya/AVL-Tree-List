107a8b355d79b972c31fddef28989d46908e16f8
Ron <ronnieyahav@gmail.com>
2024-06-20 15:04:25 +0300

    added search function and tester

--- AVLTree.py
+++ AVLTree.py
@@ -1,67 +1,86 @@
-#username - complete info
-#id1      - complete info 
-#name1    - complete info 
-#id2      - complete info
-#name2    - complete info  
-
+# username - complete info
+# id1      - complete info
+# name1    - complete info
+# id2      - complete info
+# name2    - complete info
 
 
 """A class represnting a node in an AVL tree"""
 
-class AVLNode(object):
-	"""Constructor, you are allowed to add more fields. 
-	
-	@type key: int or None
-	@param key: key of your node
-	@type value: string
-	@param value: data of your node
-	"""
-	def __init__(self, key, value):
-		self.key = key
-		self.value = value
-		self.left = None
-		self.right = None
-		self.parent = None
-		self.height = -1
-		self.size = 0
-		
 
-	"""returns whether self is not a virtual node 
+class AVLNode(object):
+    """Constructor, you are allowed to add more fields.
+
+    @type key: int or None
+    @param key: key of your node
+    @type value: string
+    @param value: data of your node
+    """
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+        self.height = -1
+        self.size = 0
+
+    """returns whether self is not a virtual node 
 
 	@rtype: bool
 	@returns: False if self is a virtual node, True otherwise.
 	"""
-	def is_real_node(self):
-		return False
 
+    def is_real_node(self):
+        if self.value == None:
+            return False
+        return True
 
 
 """
 A class implementing an AVL tree.
 """
 
+
 class AVLTree(object):
+    """
+    Constructor, you are allowed to add more fields.
 
-	"""
-	Constructor, you are allowed to add more fields.  
+    """
 
-	"""
-	def __init__(self):
-		self.root = None
+    def __init__(self):
+        self.root = None
 
-
-	"""searches for a node in the dictionary corresponding to the key
+    """searches for a node in the dictionary corresponding to the key
 
 	@type key: int
 	@param key: a key to be searched
 	@rtype: AVLNode
 	@returns: node corresponding to key
 	"""
-	def search(self, key):
-		return None
 
+    def search(self, key):
+        """helper function: gets a node and key, recursively searches for node with this key in tree
+        @type node: AVLNode, key: int
+        @param key: a key to be searched
+        @rtype AVLNode if node is in tree, else None
+        @returns: node corresponding to key
+        """
 
-	"""inserts a new node into the dictionary with corresponding key and value
+        def searchRec(node, key):
+            if node.key == None:
+                return None
+            elif node.key > key:
+                return searchRec(node.left, key)
+            elif node.key < key:
+                return searchRec(node.right, key)
+            else:
+                return node
+
+        return searchRec(self.root, key)
+
+    """inserts a new node into the dictionary with corresponding key and value
 
 	@type key: int
 	@pre: key currently does not appear in the dictionary
@@ -71,40 +90,45 @@ class AVLTree(object):
 	@rtype: int
 	@returns: the number of rebalancing operation due to AVL rebalancing
 	"""
-	def insert(self, key, val):
-		return -1
 
+    def insert(self, key, val):
+        return -1
 
-	"""deletes node from the dictionary
+    """deletes node from the dictionary
 
 	@type node: AVLNode
 	@pre: node is a real pointer to a node in self
 	@rtype: int
 	@returns: the number of rebalancing operation due to AVL rebalancing
 	"""
-	def delete(self, node):
-		return -1
 
+    def delete(self, node):
+        return -1
 
-	"""returns an array representing dictionary 
+    """returns an array representing dictionary 
 
 	@rtype: list
 	@returns: a sorted list according to key of touples (key, value) representing the data structure
 	"""
-	def avl_to_array(self):
-		return None
+
+    def avl_to_array(self): #receives tree
+        sortedArray = []
+        if self.root i
 
 
-	"""returns the number of items in dictionary 
+
+        return None
+
+    """returns the number of items in dictionary 
 
 	@rtype: int
 	@returns: the number of items in dictionary 
 	"""
-	def size(self):
-		return -1	
 
+    def size(self):
+        return self.root.size
 
-	"""compute the rank of node in the dictionary
+    """compute the rank of node in the dictionary
 
 	@type node: AVLNode
 	@pre: node is in self
@@ -112,11 +136,11 @@ class AVLTree(object):
 	@rtype: int
 	@returns: the rank of node in self
 	"""
-	def rank(self, node):
-		return -1
 
+    def rank(self, node):
+        return -1
 
-	"""finds the i'th smallest item (according to keys) in the dictionary
+    """finds the i'th smallest item (according to keys) in the dictionary
 
 	@type i: int
 	@pre: 1 <= i <= self.size()
@@ -124,11 +148,11 @@ class AVLTree(object):
 	@rtype: AVLNode
 	@returns: the node of rank i in self
 	"""
-	def select(self, i):
-		return None
 
+    def select(self, i):
+        return None
 
-	"""finds the node with the largest value in a specified range of keys
+    """finds the node with the largest value in a specified range of keys
 
 	@type a: int
 	@param a: the lower end of the range
@@ -138,14 +162,15 @@ class AVLTree(object):
 	@rtype: AVLNode
 	@returns: the node with maximal (lexicographically) value having a<=key<=b, or None if no such keys exist
 	"""
-	def max_range(self, a, b):
-		return None
 
+    def max_range(self, a, b):
+        return None
 
-	"""returns the root of the tree representing the dictionary
+    """returns the root of the tree representing the dictionary
 
 	@rtype: AVLNode
 	@returns: the root, None if the dictionary is empty
 	"""
-	def get_root(self):
-		return None
+
+    def get_root(self):
+        return None
